d9a5cf9bf43223da70af6f7c9f30bac9c9a212fd
Alexei Mamani <alexeim763@gmail.com>
2020-09-04 11:22:18 -0500

    :recycle: Refactor MainView

--- src/views/MainView/index.js
+++ src/views/MainView/index.js
@@ -1,29 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react'
 import Card from '../../Components/UI/Cards/index'
 import { CardContent } from './styles'
 import { Layout } from 'antd'
 import products from '../../db.json'
 
-
 const MainView = () => {
+  return (
+    <Layout>
+      <CardContent>
+        {
+          products.map((product, index) => {
+            return (
+              <Card key={index} product={product}/>
+            )
+          })
+        }
+      </CardContent>
+    </Layout>
 
-
-
-
-    return (
-        <Layout>
-            <CardContent>
-                {
-                    products.map(product => {
-                        return (
-                          <Card product={product}/>
-                        )
-                    })
-                }
-            </CardContent>
-        </Layout>
-
-    );
+  )
 }
 
-export default MainView;
+export default MainView
