fcd15ab7fc2c1859d7e2a6ffc507d3d5b2a8eb8e
Alexei Mamani <alexeim763@gmail.com>
2020-09-04 11:04:36 -0500

    ♻️ Refactor Card Component

---
+++ src/Components/Card/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Container, Button, Image } from './styles'
+import PropTypes from 'prop-types';
+
+
+const Card = (props) => {
+
+  const {
+    title = 'Sin Información',
+    subtitle = '-'
+  } = props
+
+  return (
+    <Container>
+      <Image></Image>
+      row
+        column
+          Info
+        column
+          Info
+        column
+          Info
+      <Button> { title } </Button>
+      { subtitle}
+    </Container>
+  )
+}
+
+Card.propTypes = {
+  title: PropTypes.string
+}
+
+export default Card
+
---
+++ src/Components/Card/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 200px;
+  height: 300px;
+  background-color: #f7a5a5;
+  display: flex;
+  flex-direction: column;
+`
+
+export const Button = styled.a`
+  padding: 10px 32px;
+  background-color: white;
+  color: black;
+  box-shadow: 4px 5px #0000003d;
+`
+
+export const Image = styled.img`
+  width: 100px;
+  height: 140px;
+  background-color: grey;
+`
+
+
